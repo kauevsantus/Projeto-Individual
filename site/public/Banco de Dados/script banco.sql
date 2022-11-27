1e59e0b904c5a010cbde0def375de4a41ffcd3f0
kauevsantus <kaue.vsantos@sptech.school>
2022-11-27 03:59:27 -0300

    atualizando script do banco de dados

--- site/public/Banco de Dados/script banco.sql
+++ site/public/Banco de Dados/script banco.sql
@@ -1,8 +1,15 @@
-
 create database taylorswift;
 use taylorswift;
+drop database taylorswift;
 
+-- Table Album
+create table  Album (
+  idAlbum int primary key auto_increment,
+  nomeAlbum varchar(45),
+  foto varchar(300)
+);
 
+select * from album;
 -- Table Usuario
 create table Usuario (
   idUsuario int primary key auto_increment,
@@ -12,27 +19,21 @@ create table Usuario (
   );
 select * from Usuario;
 
+
 -- Table Quiz
 
 create table Quiz (
   idQuiz int primary key auto_increment,
   qtdPontos int,
   fkUsuario int,
-    foreign key (fkUsuario)
-    references Usuario (idUsuario)
+  foreign key (fkUsuario)
+    references Usuario (idUsuario),
+  fkAlbum int,
+  foreign key (fkAlbum)
+    references Album (idAlbum)
    ); 
-  
 
--- Table Album
-
-create table  Album (
-  idAlbum int primary key auto_increment,
-  nome varchar(45),
-  fkQuiz int,
-    foreign key (fkQuiz)
-    references Quiz (idQuiz)
-);
-
+select * from quiz;
 
 -- Table Musicas
 
@@ -45,4 +46,43 @@ create table Musicas (
     references Album (idAlbum)
 );
 
+desc album;
+select * from album;
+insert into Album values
 
+	(null, 'Taylor Swift', 'assets/imgs/taylor swift.jfif'),
+    (null, 'Fearless', 'assets/imgs/fearless.jfif'),
+    (null, 'Speak Now', 'assets/imgs/speak now.jfif'),
+    (null, 'RED', 'assets/imgs/red.jfif'),
+    (null, '1989', 'assets/imgs/1989.jfif'),
+    (null, 'Reputation', 'assets/imgs/reputation.jfif'),
+    (null, 'Lover', 'assets/imgs/lover.jfif'),
+    (null, 'Folklore', 'assets/imgs/folklore.jfif'),
+    (null, 'Evermore', 'assets/imgs/evermore.jfif'),
+    (null, 'Midnights', 'assets/imgs/midnigths.jfif');
+    
+
+desc Musicas;
+select * from Musicas;
+
+insert into Musicas values
+
+-- singles 
+(null, 'Our Song', 1, 1),
+(null, 'Love Story', 1, 2),
+(null, 'Mine', 1, 3),
+(null, 'We Are Never Ever Getting Back Together', 1, 4),
+(null, 'Shake It Off', 1, 5),
+(null, 'Look What You Made Me Do', 1, 6),
+(null, 'ME!', 1, 7),
+(null, 'Cardigan', 1, 8),
+(null, 'Willow', 1, 9),
+(null, 'Anti-Hero', 1, 10);
+
+
+
+-- teste select para API
+select u.idUsuario , q.qtdPontos, a.foto, a.nomeAlbum, m.nome from Usuario as u 
+	join Quiz as q on fkUsuario = idUsuario
+		join Album as a on fkAlbum = idAlbum
+			join Musicas as m on m.fkAlbum = idAlbum where idUsuario = 3;
